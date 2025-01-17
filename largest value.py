1f9570053ba27c7eb7ffb5d6989c6fbf5fc99d36
VASANTH S <134638576+vasanthvnr@users.noreply.github.com>
2025-01-17 21:59:21 +0530

    Delete largest value.py

--- largest value.py
+++
@@ -1,9 +0,0 @@
-def large(n):
-    largest=0
-    for i in n:
-        if i>largest:
-            second, largest=largest, i
-        elif i>second and n!=largest:
-            second =i
-    return second
-print(large([1,3,5,2,0]))
