2229b12027c0f60487ba04c211b5df7c9f03b94b
VASANTH S <134638576+vasanthvnr@users.noreply.github.com>
2025-01-17 22:02:51 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -4,3 +4,6 @@ def fibonacci(n):
         fib.append(fib[i-1]+fib[i-2])
     return fib
 print(fibonacci(5))
+
+
+#fibonacci is to add the two preceding numbers to get the third number upto the given range. it starts from 0,1.
