727c6d0e763da159d2ec3bd22b5dd0ce911d0325
VASANTH S <134638576+vasanthvnr@users.noreply.github.com>
2025-01-17 22:06:24 +0530

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -4,3 +4,8 @@ def year(n):
     else:
         return False
 print(year(2000))
+
+
+#leap year to find the year would be leap year or not
+
+# to modulus the year by 4 and 100 or 400 to get the result as 0 is a leap year otherwise not a leapyear
