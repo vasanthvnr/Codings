957ce5cb3ee2154a5d58d2daeaa981ce34e35e21
VASANTH S <134638576+vasanthvnr@users.noreply.github.com>
2025-01-17 21:54:46 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -14,3 +14,7 @@ if m==temp :
 else:
     print("Not armstrong")
 
+#In the above we have to solve the armstrong of the number and say the number will be armstrong or not
+
+#to declare the variables
+#rmstrong concept is to split the digits in a integer power to the length of the string and sum these to get as same as the input integer is called as armstrong number
