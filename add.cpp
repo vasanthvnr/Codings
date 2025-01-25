d00f357cdbccd700494fba476e56a5fd64f36c69
VASANTH S <134638576+vasanthvnr@users.noreply.github.com>
2025-01-25 22:34:49 +0530

    Delete add.cpp

--- add.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-  int a,b;
-  cout<<"Enter the two numbers:";
-  cin >> a,b;
-  cout<<"The sum of two numbers"<< a <<"&"<<b<< "is"<<a+b;
-  return 0;
-}
