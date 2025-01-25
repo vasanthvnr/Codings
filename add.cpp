350fbb3c767b7c7bc7d7b1175a6a2999d2bf7b65
VASANTH S <134638576+vasanthvnr@users.noreply.github.com>
2025-01-25 22:15:58 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int a,b;
+  cout<<"Enter the two numbers:";
+  cin >> a,b;
+  cout<<"The sum of two numbers"<< a <<"&"<<b<< "is"<<a+b;
+  return 0;
+}
