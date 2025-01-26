6301322021db28ef96a971a0d9137d1a4f066236
VASANTH S <134638576+vasanthvnr@users.noreply.github.com>
2025-01-26 18:58:34 +0530

    Add files via upload

---
+++ c++/sum-of-n-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int sum= n*n-1/2;
+    cout<<"The Sum of "<<n<< " number is : "<<sum<<endl;
+    return 0;
+}
+
+//by using the formula to add the n number of values
