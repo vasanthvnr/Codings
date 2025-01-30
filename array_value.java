93a8e0e96045d542f216e32490315b92d7684271
VASANTH S <134638576+vasanthvnr@users.noreply.github.com>
2025-01-30 22:30:31 +0530

    array_value.java

---
+++ array_value.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class array {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int[] a=input.nextInt();
+        int result=0;
+        for(int num:a){
+            result^=num;
+//x or to get the non repeated value or character
+        }
+        System.out.println(result);
+        }
+
+    }
