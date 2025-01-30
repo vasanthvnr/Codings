2c29fe080d6d24b9c2b27dc9ef704ee6bb11ae92
VASANTH S <134638576+vasanthvnr@users.noreply.github.com>
2025-01-30 19:02:43 +0530

    Update cheerful.java

--- cheerful.java
+++ cheerful.java
@@ -2,6 +2,7 @@ public class cheerful {
     public static void main(String[] args) {
         String s= "ALL IS WELL";
         for (int i=1;i<=20;i++){
+            //print the message upto limit that is 20
             System.out.println(s);
         }
     }
