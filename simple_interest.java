b0c748750993865235ec4cbf33de850a083c6d73
VASANTH S <134638576+vasanthvnr@users.noreply.github.com>
2025-02-05 18:21:29 +0530

    Add files via upload

---
+++ simple_interest.java
@@ -0,0 +1,8 @@
+public class simple_interest {
+    public static void main(String[] args) {
+        int p=10000,r=5,t=5;
+        int si=0;
+        si=(p*t*r)/100;
+        System.out.println("Simple Interest is :"+si);
+    }
+}
